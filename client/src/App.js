85e061a864f5945263cc12a111711cbd000b63ea
Alex Nistor <alex.nistor@hpm.ro>
2023-08-04 09:45:05 +0300

    feat: add gitignore to project

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
-import logo from "./logo.svg";
-import "./App.css";
+import logo from "./logo.svg"
+import "./App.css"
 
 function App() {
-  return <div className="App"></div>;
+    return <div className="App">Hello World!</div>
 }
 
-export default App;
+export default App
