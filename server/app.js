85a4eb45a6f1725531f0e8a816d6f7872acc051d
Alex Nistor <alex.nistor@hpm.ro>
2023-08-04 09:39:05 +0300

    chore: change prettier settings

--- server/app.js
+++ server/app.js
@@ -1,12 +1,11 @@
-// We can also change the whole app to ES6 syntax but it will be like this for the task
-const http = require("http");
+const http = require("http")
 
-const hostname = "127.0.0.1";
+const hostname = "127.0.0.1"
 
-const port = 3000;
+const port = 3000
 
-const server = require("./route.js");
+const server = require("./route.js")
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+    console.log(`Server running at http://${hostname}:${port}/`)
+})
