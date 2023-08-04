10da14eb383aed98c6f5add256831aa7e85f0e5a
Alex Nistor <alex.nistor@hpm.ro>
2023-08-04 15:23:44 +0300

    feat: add more typescript stuff

--- client/src/Typescript.tsx
+++ client/src/Typescript.tsx
@@ -18,12 +18,25 @@ interface user {
     email: string
 }
 
+function showUser(user: user, message?: string): string {
+    return `${message} ${user.name} ${user.age} ${user.email}`
+}
+
+function showGreeting(message: string = "Good day, sir"): void {
+    console.log(message)
+}
+
 const Typescript = () => {
+    const scores: number[] = [1, 2, 3, 4, 5]
+    let highScores: number[]
+    highScores = scores.filter((score) => score > 3)
     const user: user = {
         name: "Huy",
         age: 20,
         email: "tttt@hpm.com",
     }
+    console.log(showUser(user, "Hello"))
+    showGreeting("Hello")
     const car = new Car("BMW", 100000)
     console.log(car.showPrice())
     return (
